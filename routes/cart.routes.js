f4cf72201bf72b246499c9e1267ca740f0269984
behhruz05 <sotimboyevbehruz85@gmail.com>
2026-02-12 15:38:20 +0500

    home page done

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,3 +1,26 @@
+import express from "express";
+import { getCart, addToCart } from "../controllers/cart.controller.js";
+import { protect } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * tags:
+ *   name: Cart
+ */
+
+/**
+ * @swagger
+ * /api/cart:
+ *   get:
+ *     summary: Get user cart
+ *     tags: [Cart]
+ *     security:
+ *       - BearerAuth: []
+ */
+router.get("/", protect, getCart);
+
 /**
  * @swagger
  * /api/cart/add:
@@ -19,3 +42,7 @@
  *                 type: number
  */
 router.post("/add", protect, addToCart);
+
+
+
+export default router;
